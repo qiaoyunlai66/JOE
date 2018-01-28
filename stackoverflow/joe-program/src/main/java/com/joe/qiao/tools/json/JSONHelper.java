1f7867b4c6be13051dbf0ca50ad2479d03991630
joe.qiao <joeqiao@fortinet.com>
2018-01-28 19:55:55 +0800

    JSONHelper.java

--- stackoverflow/joe-program/src/main/java/com/joe/qiao/tools/json/JSONHelper.java
+++ stackoverflow/joe-program/src/main/java/com/joe/qiao/tools/json/JSONHelper.java
@@ -1,11 +1,13 @@
 package com.joe.qiao.tools.json;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.commons.lang.StringUtils;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.annotate.JsonSerialize;
 
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,17 +15,68 @@ import java.util.List;
  * Created by Joe Qiao on 19/12/2017.
  */
 public class JSONHelper {
-    //API JACKSON
-    public static String toJson(Object obj) throws Exception{
+    /**
+     * API JACKSON
+     * @param obj
+     * @return
+     * @throws IOException
+     */
+    public static String toJsonJackSon(Object obj) throws Exception{
         if(obj == null){
             return null;
         }
         ObjectMapper mapper=new ObjectMapper();
-        mapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         String serialized = mapper.writeValueAsString(obj);
         return serialized;
     }
 
+    /**
+     * API JACKSON
+     * @param json
+     * @param clazz
+     * @param <T>
+     * @return
+     * @throws IOException
+     */
+    public static <T> T fromJsonJackSon(String json, Class<T> clazz) throws IOException {
+        ObjectMapper mapper=new ObjectMapper();
+        return mapper.readValue(json,clazz);
+    }
+
+    /**
+     * API JACKSON serialize object to json support interface
+     * @param obj
+     * @return
+     * @throws Exception
+     */
+    public static String toJsonSerialized(Object obj) throws Exception{
+        if(obj == null){
+            return null;
+        }
+        ObjectMapper mapper=new ObjectMapper();
+        mapper.enableDefaultTyping();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        String serialized = mapper.writeValueAsString(obj);
+        return serialized;
+    }
+    
+    /**
+     * API JACKSON deserialize json to object support interface
+     * @param json
+     * @param clazz
+     * @param <T>
+     * @return
+     * @throws IOException
+     */
+    public static <T> T fromJsonDeSerialized(String json, Class<T> clazz) throws IOException {
+        ObjectMapper mapper=new ObjectMapper();
+        mapper.enableDefaultTyping();
+        return mapper.readValue(json,clazz);
+    }
+
+    
+    
     /**
      * @deprecated 
      *  API gson
@@ -43,12 +96,20 @@ public class JSONHelper {
         }
         List list = JSONHelper.fromJsonForBoj(json, ArrayList.class);
         for (Object obj : list) {
-            T item = JSONHelper.fromJsonForBoj(JSONHelper.toJson(obj), clazz);
+            T item = JSONHelper.fromJsonForBoj(JSONHelper.toJsonJackSon(obj), clazz);
             ret.add(item);
         }
         return ret;
     }
-    //API gson
+
+    /**
+     * API gson
+     * @param json
+     * @param clazz
+     * @param <T>
+     * @return
+     * @throws Exception
+     */
     public static <T> T fromJsonForBoj(String json, Class<T> clazz) throws Exception {
         if(StringUtils.isBlank(json)){
             return null;
